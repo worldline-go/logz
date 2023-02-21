072ab1bbeb780d8885f7dd1e41babd003a6720c7
Eray Ates <eates23@gmail.com>
2023-02-21 15:07:35 +0100

    feat: adapter interface and noop
    Signed-off-by: Eray Ates <eates23@gmail.com>

--- adapters.go
+++ adapters.go
@@ -2,6 +2,22 @@ package logz
 
 import "github.com/rs/zerolog"
 
+type Adapter interface {
+	Error(msg string, keysAndValues ...interface{})
+	Info(msg string, keysAndValues ...interface{})
+	Debug(msg string, keysAndValues ...interface{})
+	Warn(msg string, keysAndValues ...interface{})
+}
+
+type AdapterNoop struct{}
+
+func (AdapterNoop) Error(msg string, keysAndValues ...interface{}) {}
+func (AdapterNoop) Info(msg string, keysAndValues ...interface{})  {}
+func (AdapterNoop) Debug(msg string, keysAndValues ...interface{}) {}
+func (AdapterNoop) Warn(msg string, keysAndValues ...interface{})  {}
+
+var _ Adapter = AdapterNoop{}
+
 // AdapterKV fit for msg, keyvalue interface, Ex: retryablehttp.
 //
 //	myLogFormat := log.With().Str("log_source", "mycomponent").Logger()
@@ -12,6 +28,8 @@ type AdapterKV struct {
 	FrameCount int
 	Caller     bool
 }
+
+var _ Adapter = AdapterKV{}
 
 func (l AdapterKV) frameUp() zerolog.Logger {
 	count := 3
